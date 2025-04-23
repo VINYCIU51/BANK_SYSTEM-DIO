96bc770e7e74f02f2eeffb096595eff16115d944
vinycius <vinyciusalves155@gmail.com>
2025-04-23 00:26:08 -0300

    adicionado primeira versao do botao de ver senha e cores de forca da mesma

--- app/static/js/validations/pass-validation.js
+++ app/static/js/validations/pass-validation.js
@@ -1,8 +1,7 @@
 export const passInput = document.getElementById("password");
 export const passError = document.getElementById("password-error");
-
 export const confirmPassInput = document.getElementById("confirm-password");
-export const confirmPassError = document.getElementById("confirm-password-error");
+export const toggleButton = document.querySelector(".view-password i");
 
 // verifica se o campo esta vazio
 export function emptyPass() {
@@ -16,7 +15,6 @@ export function invalidPass(menssage) {
 
     if (confirmPassInput) {
         confirmPassInput.classList.add("invalid");
-        confirmPassError.textContent = menssage;
     }
 }
 
@@ -27,7 +25,6 @@ export function passOk() {
 
     if (confirmPassInput) {
         confirmPassInput.classList.remove("invalid");
-        confirmPassError.textContent = "";
     }
 }
 
@@ -42,15 +39,46 @@ export function diferentPass(menssage) {
     passError.textContent = menssage;
 }
 
+export function passStrong() {
+    const password = passInput.value;
+    const caracteres = /[^a-zA-Z0-9]/;
+
+    if (password.length >= 8 && !caracteres.test(password)) {
+        passInput.classList.add("weak");
+        passError.textContent = "Senha media!";
+        passError.style.color = "yellow";
+
+    } else if (password.length >= 8 && caracteres.test(password)) {
+        passInput.classList.add("strong");
+        passError.textContent = "Senha forte!";
+        passError.style.color = "green";
+    }
+}
+
+export function viewPassword() {
+    if (!passInput) return;
+    passInput.type = passInput.type === "password" ? "text" : "password";
+
+    if (toggleButton) {
+        toggleButton.classList.toggle("fa-eye");
+        toggleButton.classList.toggle("fa-eye-slash");
+    }
+
+}
+
 // retira a cor vermelha ao digitar novamente
 passInput.addEventListener("input", () => {
     if (passInput.value.trim() !== "") {
         passOk();
+        passStrong();
     }
 });
 
-confirmPassInput.addEventListener("input", () => {
-    if (confirmPassInput.value.trim() !== "") {
-        confirmPassInput.classList.remove("invalid");
-    }
-});
+// retira o erro da confirmacao de senha se ela existir no DOM
+if (confirmPassInput) {
+    confirmPassInput.addEventListener("input", () => {
+        if (confirmPassInput.value.trim() !== "") {
+            passOk();
+        }
+    });
+}
