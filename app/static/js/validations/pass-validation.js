1e3fd40d4d250af2dc8987966d8bcacc2dca37b4
vinycius <vinyciusalves155@gmail.com>
2025-04-25 23:27:46 -0300

    transformacao em classe e melhoria de metodos

--- app/static/js/validations/pass-validation.js
+++ app/static/js/validations/pass-validation.js
@@ -1,108 +1,123 @@
-export const passInput = document.getElementById("password");
-export const passError = document.getElementById("password-error");
+export class PasswordValidation {
+    constructor() {
+        this.input = document.getElementById("password");
+        this.error = document.getElementById("password-error");
+        this.confirmInput = document.getElementById("confirm-password");
+        this.confirmError = document.getElementById("confirmPass-error");
 
-export const confirmPassInput = document.getElementById("confirm-password");
-export const confirmPassError = document.getElementById("confirmPass-error");
-
-export const toggleButton = document.querySelector(".view-password i");
-
-// exibe um alerta na senha
-export function passAlert(type, color, menssage) {
-    passOk(); // reseta alguma cor residual
-
-    passInput.classList.add(type);
-    passError.style.color = color;
-    passError.textContent = menssage;
-}
-
-// remove o alerta de erro da senha
-export function passOk() {
-    passInput.classList.remove("invalid", "weak", "strong");
-    passError.style.color = "black";
-    passError.textContent = "";
-
-    if (confirmPassInput) {
-        confirmPassInput.classList.remove("invalid");
-        confirmPassError.textContent = "";
-    }
-}
-
-// verifica se o campo esta vazio
-export function emptyPass() {
-    return passInput.value.trim() === "";
-}
-
-
-// verifica se o usuario digitou as confirmacao de senha corretamente
-export function isEqualPass() {
-    return passInput.value === confirmPassInput.value;
-}
-
-// exibe um alerta na senha diferente
-export function diferentPass(menssage) {
-    confirmPassInput.classList.add("invalid");
-    confirmPassError.textContent = menssage;
-}
-
-export function passStrong() {
-    const password = passInput.value;
-    const specialChars = password.match(/[^a-zA-Z0-9]/g) || [];
-    const uniqueChars = new Set(password).size;
-
-    const hasUpper = /[A-Z]/.test(password);
-    const hasLower = /[a-z]/.test(password);
-    const hasNumber = /[0-9]/.test(password);
-    const complexity = (hasUpper + hasLower + hasNumber) * 10;
-
-    const strong = ((specialChars.length / password.length) * 30) + ((uniqueChars / password.length) * 40) + complexity;
-
-    if (password.length < 8) {
-        passAlert("invalid", "red", "Mínimo 8 dígitos!");
-    } else if (strong < 40) {
-        passAlert("weak", "yellow", "Senha fraca!");
-    } else if (strong >= 40 && strong < 70) {
-        passAlert("middle", "greenyellow", "Senha média!");
-    } else {
-        passAlert("strong", "green", "Senha forte!");
-    }
-}
-
-// altera o botao e exibe ou oculta a senha
-export function viewPassword(event) {
-    const button = event.currentTarget;
-    const container = button.closest(".input-container");
-    const input = container.querySelector("input[type='password'], input[type='text']");
-
-    input.type = input.type === "password" ? "text" : "password";
-
-    const icon = button.querySelector("i");
-    icon.classList.toggle("fa-eye");
-    icon.classList.toggle("fa-eye-slash");
-}
-
-// retira a cor vermelha ao digitar novamente
-passInput.addEventListener("input", () => {
-    if (passInput.value.trim() !== "") {
-        passOk();
-        passStrong();
-    }
-});
-
-// retira o erro da confirmacao de senha se ela existir no DOM
-if (confirmPassInput) {
-
-    confirmPassInput.addEventListener("blur", () => {
-        if (passInput.value.trim() === "" && confirmPassInput.value.trim() === "") {
-            return;
+        // Configura listeners se os elementos existirem
+        if (this.input && this.error) {
+            this.setupPassListeners();
         }
-        if (!isEqualPass()) {
-            diferentPass("Senhas divergentes!");
+        if (this.confirmInput && this.confirmError) {
+            this.setupConfirmPassListeners();
         }
-    })
+    }
 
-    confirmPassInput.addEventListener("input", () => {
-        if (confirmPassInput.value.trim() !== "") {
-            passOk();
+    // exibe um alerta na senha
+    alert(type, color, menssage) {
+        this.clearAlert(); // reseta alguma cor residual
+
+        this.input.classList.add(type);
+        this.error.style.color = color;
+        this.error.textContent = menssage;
+    }
+
+    // remove o alerta de erro da senha
+    clearAlert() {
+        this.input.classList.remove("invalid", "weak", "strong");
+        this.error.style.color = "black";
+        this.error.textContent = "";
+    }
+
+    clearConfirmAlert() {
+        this.confirmInput.classList.remove("invalid");
+        this.confirmError.textContent = "";
+    }
+
+    // verifica se o campo esta vazio
+    isEmpty() {
+        return this.input.value.trim() === "";
+    }
+
+    // verifica se o usuario digitou as confirmacao de senha corretamente
+    confirmed() {
+        return this.input.value === this.confirmInput.value;
+    }
+
+    // exibe um alerta na senha diferente
+    confirmAlert(menssage) {
+        this.confirmInput.classList.add("invalid");
+        this.confirmError.textContent = menssage;
+    }
+
+    // verifica a força da senha
+    strong() {
+        const password = this.input.value;
+        const specialChars = password.match(/[^a-zA-Z0-9]/g) || [];
+        const uniqueChars = new Set(password).size;
+
+        const hasUpper = /[A-Z]/.test(password);
+        const hasLower = /[a-z]/.test(password);
+        const hasNumber = /[0-9]/.test(password);
+        const complexity = (hasUpper + hasLower + hasNumber) * 10;
+
+        // força baseada em: 35% quant de char especial + 35% quant char unicos + 30% uso de numeros,maiusculas e minusculas
+        const strong = ((specialChars.length / password.length) * 35) + ((uniqueChars / password.length) * 35) + complexity;
+
+        if (password.length < 8) {
+            this.alert("invalid", "red", "Mínimo 8 dígitos!");
+        } else if (strong < 50) {
+            this.alert("weak", "yellow", "Senha fraca!");
+        } else if (strong >= 50 && strong < 70) {
+            this.alert("middle", "greenyellow", "Senha média!");
+        } else {
+            this.alert("strong", "green", "Senha forte!");
         }
-    });
+    }
+
+    // altera o botao e exibe ou oculta a senha
+    viewPassword(event) {
+        const button = event.currentTarget;
+        const container = button.closest(".input-container");
+        const input = container.querySelector("input[type='password'], input[type='text']");
+
+        input.type = input.type === "password" ? "text" : "password";
+
+        const icon = button.querySelector("i");
+        icon.classList.toggle("fa-eye");
+        icon.classList.toggle("fa-eye-slash");
+    }
+
+    // Configura listeners para o campo de senha
+    setupPassListeners() {
+        // Monitora cada digito no campo senha
+        this.input.addEventListener("input", () => {
+            if (this.input.value.trim() === "") {
+                return;
+            }
+            this.clearAlert();
+        });
+    }
+
+    // Configura listeners para o campo de confirmação de senha
+    setupConfirmPassListeners() {
+        // monitora o campo de confirmacao de senha
+        this.confirmInput.addEventListener("blur", () => {
+            if (this.input.value.trim() === "" && this.confirmInput.value.trim() === "") {
+                return;
+            }
+
+            if (!this.confirmed()) {
+                this.confirmAlert("Senhas divergentes!");
+            }
+        });
+
+        this.confirmInput.addEventListener("input", () => {
+            if (this.confirmInput.value.trim() === "") {
+                return;
+            }
+            this.clearConfirmAlert();
+        });
+    }
 }
