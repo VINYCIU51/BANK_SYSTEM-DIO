a0fe7960ecd57564093cd616a38df4b56856b301
vinycius <vinyciusalves155@gmail.com>
2025-05-12 22:52:34 -0300

    ajuste da tabela

--- app/models/data-base.py
+++ app/models/data-base.py
@@ -6,17 +6,18 @@ cursor = conn.cursor()
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS usuarios(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome_completo TEXT NOT NULL,
-    nome_mae TEXT,
-    cpf TEXT NOT NULL UNIQUE,
-    nacionalidade TEXT,
-    cep TEXT NOT NULL,
-    data_nascimento DATE NOT NULL,
-    telefone TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
-    senha TEXT NOT NULL,
-    saldo REAL NOT NULL DEFAULT 0.0,
-    limite_credito REAL DEFAULT 500.0,      
+    id INTEGER PRIMARY KEY AUTOINCREMENT,  
+    name TEXT NOT NULL,  
+    mother_name TEXT,  
+    cpf TEXT NOT NULL UNIQUE,  
+    nationality TEXT,  
+    zip_code TEXT NOT NULL,  
+    birth_date DATE NOT NULL,  
+    phone_number TEXT NOT NULL,  
+    email TEXT NOT NULL UNIQUE,  
+    password TEXT NOT NULL,  
+    account_number TEXT UNIQUE,  
+    balance REAL NOT NULL DEFAULT 0.0,  
+    credit_limit REAL DEFAULT 500.0      
 )
 """)
